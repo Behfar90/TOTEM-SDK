a91688b379513f807abef1469bcf75f918e965c6
behfar <behfar@origin.no>
2020-06-29 14:41:20 +0200

    bug fixed: apply naming errors to equations

--- src/index.js
+++ src/index.js
@@ -10883,69 +10883,73 @@ let handleNumber = function handleNumber(chunk, cmd) {
     if (chunk.includes('=')){
         let leftSideOfEquition = chunk.substr(0, chunk.indexOf('='))
         let righSideOfEquition = chunk.substr(chunk.indexOf("=") + 1)
-        let isOperation = opsDetector(righSideOfEquition, Object.keys(ops)) // if it is an operation
-        let noError = true;
-        if ( !isNaN(parseInt(righSideOfEquition)) ) {  // if it is a number
-            if(cmd == "int") {
-                TOTEM_operators.push('INTassign')
-                mapped_executions.push(cmd + ' ' + chunk)
-                userDefined_vars[leftSideOfEquition] = parseInt(righSideOfEquition)
-            } else {
-                TOTEM_operators.push('FLOATassign')
-                mapped_executions.push(cmd + ' ' + chunk)
-                userDefined_vars[leftSideOfEquition] = parseFloat(righSideOfEquition)
-            }
-        }
-        else if( isOperation[0] ) {
-            
-            var arguments = righSideOfEquition.match(/\((.*)\)/).pop().split(',')
-            if (arguments.length == 2) { // number of arguments should be exactly 2
-                arguments.forEach( function(arg, i) {
-                    if ( isNaN(arg) ) {
-                        if (Object.keys(userDefined_vars).includes(arg)) {
-                            // to give the argument the value ut got before by user
-                            this[i] = userDefined_vars[arg]
-                        }
-                        else {
-                            errorHandler.operationError('undefinedVar',arg)
-                            noError = false // to stop the rest from executing
-                        }
-                    }
-                }, arguments);
-                // console.log(arguments)
-                if (noError) {
-                   switch (isOperation[1]) {
-                    case "add":
-                        userDefined_vars[leftSideOfEquition] = ops.add(arguments[0],arguments[1])
-                        break;
-                    case "sub":
-                        userDefined_vars[leftSideOfEquition] = ops.sub(arguments[0],arguments[1])
-                        break;
-                    case "mul":
-                        userDefined_vars[leftSideOfEquition] = ops.mul(arguments[0],arguments[1])
-                        break;
-                    case "div":
-                        userDefined_vars[leftSideOfEquition] = ops.div(arguments[0],arguments[1])
-                        break;
-                    case "pow":
-                        userDefined_vars[leftSideOfEquition] = ops.pow(arguments[0],arguments[1])
-                        break;
-
-                    }
-                    TOTEM_operators.push('MathOperation')
-                    mapped_executions.push(leftSideOfEquition + '=' + righSideOfEquition) 
+        if (notAllowedNamingConvention.some(el => leftSideOfEquition.includes(el))) {
+            console.log('miad inja')
+            errorHandler.namingError(leftSideOfEquition)
+        } else {
+            let isOperation = opsDetector(righSideOfEquition, Object.keys(ops)) // if it is an operation
+            let noError = true;
+            if ( !isNaN(parseInt(righSideOfEquition)) ) {  // if it is a number
+                if(cmd == "int") {
+                    TOTEM_operators.push('INTassign')
+                    mapped_executions.push(cmd + ' ' + chunk)
+                    userDefined_vars[leftSideOfEquition] = parseInt(righSideOfEquition)
+                } else {
+                    TOTEM_operators.push('FLOATassign')
+                    mapped_executions.push(cmd + ' ' + chunk)
+                    userDefined_vars[leftSideOfEquition] = parseFloat(righSideOfEquition)
                 }
+            }
+            else if( isOperation[0] ) {
                 
+                var arguments = righSideOfEquition.match(/\((.*)\)/).pop().split(',')
+                if (arguments.length == 2) { // number of arguments should be exactly 2
+                    arguments.forEach( function(arg, i) {
+                        if ( isNaN(arg) ) {
+                            if (Object.keys(userDefined_vars).includes(arg)) {
+                                // to give the argument the value ut got before by user
+                                this[i] = userDefined_vars[arg]
+                            }
+                            else {
+                                errorHandler.operationError('undefinedVar',arg)
+                                noError = false // to stop the rest from executing
+                            }
+                        }
+                    }, arguments);
+                    // console.log(arguments)
+                    if (noError) {
+                    switch (isOperation[1]) {
+                        case "add":
+                            userDefined_vars[leftSideOfEquition] = ops.add(arguments[0],arguments[1])
+                            break;
+                        case "sub":
+                            userDefined_vars[leftSideOfEquition] = ops.sub(arguments[0],arguments[1])
+                            break;
+                        case "mul":
+                            userDefined_vars[leftSideOfEquition] = ops.mul(arguments[0],arguments[1])
+                            break;
+                        case "div":
+                            userDefined_vars[leftSideOfEquition] = ops.div(arguments[0],arguments[1])
+                            break;
+                        case "pow":
+                            userDefined_vars[leftSideOfEquition] = ops.pow(arguments[0],arguments[1])
+                            break;
+
+                        }
+                        TOTEM_operators.push('MathOperation')
+                        mapped_executions.push(leftSideOfEquition + '=' + righSideOfEquition) 
+                    }
+                    
+                }
+                else {
+                    errorHandler.operationError('argNumberError',isOperation[1])
+                }
             }
             else {
-                errorHandler.operationError('argNumberError',isOperation[1])
+                // passing the incorrect function name into operation error handler
+                errorHandler.operationError('noSuchOperation',isOperation[1])
             }
         }
-        else {
-            // passing the incorrect function name into operation error handler
-            errorHandler.operationError('noSuchOperation',isOperation[1])
-        }
-
     } else {
             notAllowedNamingConvention.some(el => chunk.includes(el))
             ?   errorHandler.namingError(chunk)
